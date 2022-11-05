84912e40745b3437dd491412d31a82aaddee2f49
haith <haithemmihoubi1234@gmail.com>
2022-11-05 22:42:50 +0100

    posts table entity jpa

---
+++ src/main/java/com/haithem/blog/entity/Post.java
@@ -0,0 +1,45 @@
+package com.haithem.blog.entity;
+
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "posts", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {
+                "title"
+        })
+})
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "title", nullable = false, length = 100)
+    private String title;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "content")
+    private String content;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Post post = (Post) o;
+        return id != null && Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
